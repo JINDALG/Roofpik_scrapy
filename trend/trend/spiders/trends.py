789d7b154b6dff8248f13448a85361a61f57e5ec
jindalg <jindalshivam65@gmail.com>
2016-06-16 19:17:59 +0530

    trends permutations complete

--- trend/trend/spiders/trends.py
+++ trend/trend/spiders/trends.py
@@ -13,6 +13,7 @@ import time
 from Tkinter import *
 from pprint import pprint
 import csv
+import itertools
 
 
 class Result(scrapy.Spider):
@@ -23,7 +24,7 @@ class Result(scrapy.Spider):
 
     def fetch(self,entries):
         sroll = entries[0]
-        self.text = '+'.join((entries[0][1].get()).split())
+        self.text = (entries[0][1].get()).split()
         self.root.destroy()
 
     def makeform(self,fields):
@@ -63,16 +64,25 @@ class Result(scrapy.Spider):
 
     def parse(self, response):
         
-        self.driver.get('https://www.google.co.in/?gfe_rd=cr&ei=cUFiV5C4Lu-q8weqqI-oAQ&gws_rd=ssl#q=%s' %(self.text))
-        try:
-            WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.XPATH, '//h3//a')))
-        except TimeoutException:
-            return
-        resp = TextResponse(url=self.driver.current_url, body=self.driver.page_source, encoding='utf-8');
+        sent = (self.text)
+        all_permut = list(itertools.permutations(sent))
+        pprint(all_permut)
+        input()
 
-        urls = resp.xpath('//h3//a/@href').extract()
-        for url in urls :
-            yield scrapy.Request(url, callback = self.parse_page)
+        for permut in all_permut:
+            text = '+'.join(permut)
+            url = 'https://www.google.co.in/?gfe_rd=cr&ei=cUFiV5C4Lu-q8weqqI-oAQ&gws_rd=ssl#q=%s' %(text)
+            self.driver.get(url)
+            try:
+                WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.XPATH, '//h3//a')))
+            except TimeoutException:
+                return
+            resp = TextResponse(url=self.driver.current_url, body=self.driver.page_source, encoding='utf-8');
+
+            urls = resp.xpath('//h3//a/@href').extract()
+            pprint(urls)
+            for url in urls :
+                yield scrapy.Request(url, callback = self.parse_page)
 
 
 
@@ -84,7 +94,8 @@ class Result(scrapy.Spider):
         except TimeoutException:
             pass
         resp = TextResponse(url=self.driver.current_url, body=self.driver.page_source, encoding='utf-8');
-        temp['p'] = resp.xpath('//p//text()').extract()
+
+        temp['p'] = ((('abcd'.join(resp.xpath('//p//text()').extract())).replace('\n','').replace('\t','')).encode('utf8')).split('abcd')
         temp['a'] = resp.xpath('//a//text()').extract()
         temp['span'] = resp.xpath('//span//text()').extract()
         temp['h1'] = resp.xpath('//h1//text()').extract()
@@ -99,12 +110,28 @@ class Result(scrapy.Spider):
         temp['img'] = resp.xpath('//img/@alt').extract()
         temp['th'] = resp.xpath('//th//text()').extract()
         temp['td'] = resp.xpath('//td//text()').extract() 
+
+        for item in temp:
+            try :
+                temp[item] = (('abcd'.join(temp[item])).replace('\n','').replace('\t','').replace(',',' '))
+                temp[item] = re.sub(' +',' ',temp[item])
+                temp[item] = (''.join([i if ord(i) < 128 else '' for i in temp[item]])).encode('utf8').split('abcd')
+
+            except :
+                pass
         temp['url'] = response.url
 
-        with open('data.csv','ab') as csvfile:
+        with open('data.csv','ab+') as csvfile:
             fieldnames = ['url','p','a','span','h1','h2','h3','h4','h5','h6','title','meta','li','img','th','td']
             writer = csv.DictWriter(csvfile, fieldnames)
-            writer.writerow(temp)
+            reader = csv.DictReader(csvfile)
+            flag = True
+            for row in reader:
+                if row['url'] == temp['url']:
+                    flag = False
+                    break
+            if flag:
+                writer.writerow(temp)
 
         pprint(temp)
 
