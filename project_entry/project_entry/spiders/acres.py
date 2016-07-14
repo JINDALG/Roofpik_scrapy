97a990c37d7e9a6e4dc1cc71225d3bc7e7626473
jindalg <jindalshivam65@gmail.com>
2016-07-14 18:44:47 +0530

    99acres done

--- project_entry/project_entry/spiders/acres.py
+++ project_entry/project_entry/spiders/acres.py
@@ -15,8 +15,10 @@ from firebase import firebase
 class acres99(scrapy.Spider):
 	name = "99acres"
 	allowed_domains = ["99acres.com"]   # target site
-	start_urls = ['http://www.99acres.com/vipul-greens-sector-48-gurgaon-npxid-r12234?sid=UiB8IFFTIHwgUyB8IzcjICB8IENQMSB8IFkgIzE2I3wgIHwgMSM0IyB8IDEgfCA4ICM1I3wgIHwgMSMzNiMgfCA2NjgjNCMgfCA=']
+	start_urls = None
 	def __init__(self, filename=None):
+		with open('link.txt','r') as f:
+			self.start_urls = [f.read()]
 		self.driver = webdriver.Chrome()
 		dispatcher.connect(self.spider_closed, signals.spider_closed)
 
@@ -25,8 +27,9 @@ class acres99(scrapy.Spider):
 
 	def parse(self,response):
 		item = {}
-		min_price = min_area = max_price = max_area = area =  -1
-		status = apartment_type = apartment_bhk = "" 
+		min_booking_price = min_area = max_booking_price = max_area = area = max_resale_price = min_resale_price= -1
+		status = apartment_type = apartment_bhk = project_detail= builderName= "" 
+		superBuiltupArea = builtupArea = min_book_price = max_book_price = min_sale_price = max_sale_price = bhk = -1
 		try :
 			name = ''.join(response.xpath('//div[@class="bannerOver"]//span[@itemprop="name"]//text()').extract())
 		except:
@@ -38,7 +41,7 @@ class acres99(scrapy.Spider):
 				try :
 					head = ''.join(fact.xpath('div[contains(@class,"factData")]//div[contains(@class,"factLbl")]//text()').extract())
 					if "Possession" in head:
-						status =' '.join(fact.xpath('div[contains(@class,"factData")]//div[contains(@class,"factVal")]//text()').extract())
+						status =' '.join(fact.xpath('div[contains(@class,"factData")]//div[contains(@class,"factVal1")]//text()').extract())
 					
 					if "Address" in head:
 						addres =' '.join(fact.xpath('div[contains(@class,"factData")]//div[contains(@class,"factVal")]//text()').extract())
@@ -49,7 +52,7 @@ class acres99(scrapy.Spider):
 					
 					if "Total Project Area" in head:
 						area =''.join(fact.xpath('div[contains(@class,"factData")]//div[contains(@class,"factVal")]//text()').extract()).replace('Acres','')
-						area  = float(area)
+						area  = float(area)*43560
 					
 					if "Saleable Area" in head:
 						areas = ''.join(fact.xpath('div[contains(@class,"factData")]//div[contains(@class,"factVal")]//text()').extract())
@@ -62,12 +65,12 @@ class acres99(scrapy.Spider):
 						price = (re.sub(r'[^\x00-\x7F]', " ", price))
 						iscr = 'Crore' in price
 						islac = 'Lac' in price
-						price = price.replace('Crore','').replace('Lac','').split('to')
-						min_price,max_price = map(float,price.split('to'))
-						min_price *= 10000000 if iscr else 1
-						max_price *= 10000000 if iscr else 1
-						min_price *= 100000 if islac else 1
-						min_price *= 100000 if islac else 1
+						price = price.replace('Crore','').replace('Lac','').strip()
+						min_resale_price,max_resale_price = map(float,price.split('to'))
+						min_resale_price *= 10000000 if iscr else 1
+						max_resale_price *= 10000000 if iscr else 1
+						min_resale_price *= 100000 if islac else 1
+						min_resale_price *= 100000 if islac else 1
 
 					if "New Booking Base Price" in head:
 						price =''.join(fact.xpath('div[contains(@class,"factData")]//div[contains(@class,"factVal")]//text()').extract())
@@ -75,11 +78,16 @@ class acres99(scrapy.Spider):
 						iscr = 'Crore' in price
 						islac = 'Lac' in price
 						price = price.replace('Crore','').replace('Lac','').strip()
-						min_price,max_price = map(float,price.split('to'))
-						min_price *= 10000000 if iscr else 1
-						max_price *= 10000000 if iscr else 1
-						min_price *= 100000 if islac else 1
-						min_price *= 100000 if islac else 1
+						min_booking_price, max_booking_price = map(float,price.split('to'))
+						min_booking_price *= 10000000 if iscr else 1
+						max_booking_price *= 10000000 if iscr else 1
+						min_booking_price *= 100000 if islac else 1
+						min_booking_price *= 100000 if islac else 1
+
+					if "Project Details" in head:
+						project_detail =' '.join(fact.xpath('div[contains(@class,"factData")]//div[contains(@class,"factVal")]//text()').extract())
+						project_detail = (re.sub(r'[^\x00-\x7F]', " ", project_detail))
+						
 				except:
 					pass
 		except :
@@ -93,7 +101,6 @@ class acres99(scrapy.Spider):
 			for bas in basic:
 				try :
 					amen = ''.join(bas.xpath('div/text()').extract())
-					print amen
 					amenity += [amen.encode('utf8')]
 				except:
 					pass
@@ -116,53 +123,87 @@ class acres99(scrapy.Spider):
 				try :
 					try :
 						bhk = ''.join(detail.xpath('div[@class="head"]/div//text()').extract())[0].encode('utf8')
+						bhk = int(bhk.replace('BHK Apartment',''))
 					except :
 						pass
-					try :
-						superbuiltarea = float(''.join(detail.xpath('div[2]/div[2]/text()').extract()))
-					except :
-						pass
-					try :
-						builtuparea = float(''.join(detail.xpath('div[5]/div[2]/text()').extract()))
-					except :
-						pass
-					try :
-						price =(''.join(detail.xpath('div[8]/div[2]/text()').extract()))
-						price = (re.sub(r'[^\x00-\x7F]', " ", price))
-						iscr = 'Crore' in price
-						islac = 'Lac' in price
-						price = price.replace('Crore','').replace('Lac','').strip().split('-')
+
+					more_det = detail.xpath('div[@class="unit-d-cs"]')
+					for internal_det in more_det:
 						try :
-							min_price = float(price[0])
+							if "super built-up area" in ''.join(internal_det.xpath('div[1]/text()').extract()):
+								superBuiltupArea = (''.join(internal_det.xpath('div[2]/text()').extract())).encode('utf8')
+
+							if "Built-up area" in ''.join(internal_det.xpath('div[1]/text()').extract()):
+								builtupArea = (''.join(internal_det.xpath('div[2]/text()').extract())).encode('utf8')
+
+							if "New Booking Base Price" in ''.join(internal_det.xpath('div[1]/text()').extract()):
+								price = (''.join(internal_det.xpath('div[2]/text()').extract()))
+								iscr = 'Crore' in price
+								islac = 'Lac' in price
+								price = price.replace('Crore','').replace('Lac','')
+								if '-' in price:
+									price = price.split('-')
+									min_book_price = float(price[0])
+									max_book_price = float(price[1])
+								else :
+									min_book_price = max_book_price = float(price)
+								min_book_price *= 10000000 if iscr else 1
+								max_book_price *= 10000000 if iscr else 1
+								min_book_price *= 100000 if islac else 1
+								max_book_price *= 100000 if islac else 1
+
+							if "Resale Price" in ''.join(internal_det.xpath('div[1]/text()').extract()):
+								price = (''.join(internal_det.xpath('div[2]/text()').extract()))
+								iscr = 'Crore' in price
+								islac = 'Lac' in price
+								price = price.replace('Crore','').replace('Lac','')
+								if '-' in price:
+									price = price.split('-')
+									min_sale_price = float(price[0])
+									max_sale_price = float(price[1])
+								else :
+									min_sale_price = max_sale_price = float(price)
+								min_sale_price *= 10000000 if iscr else 1
+								max_sale_price *= 10000000 if iscr else 1
+								min_sale_price *= 100000 if islac else 1
+								max_sale_price *= 100000 if islac else 1
 						except:
-							min_price  = 0
-						try :
-							max_price = float(price[1])
-						except :
-							max_price = 0
-						min_price *= 10000000 if iscr else 1
-						max_price *= 10000000 if iscr else 1
-						min_price *= 100000 if islac else 1
-						min_price *= 100000 if islac else 1
-					except :
-						pass
-					det += [{'bhk':bhk,'min_price':min_price,'max_price':max_price,'superBuiltArea':superbuiltarea,'builtupArea':builtuparea,}]
+							pass
+					det += [{'bhk':bhk,
+					'superBuiltupArea':superBuiltupArea,
+					'builtupArea':builtupArea,
+					'min_book_price':min_book_price,
+					'max_book_price':max_book_price,
+					'min_sale_price':min_sale_price,
+					'max_sale_price':max_sale_price}]
 				except :
 					pass
 		except :
 			pass
 
+		try :
+			builderName = ''.join(resp.xpath('//span[@id="item_manufacturer"]//text()').extract())
+		except :
+			pass
 		item['url'] = response.url
-		item['name'] = name.encode('utf8')
+		item['projectName'] = name.encode('utf8')
 		item['status'] = status.encode('utf8')
-		item['type'] = apartment_type.encode('utf8')
+		item['projectType'] = apartment_type.encode('utf8')
 		item['bhk'] = apartment_bhk.encode('utf8')
 		item['area'] = area
 		item['min_area'] = min_area
 		item['max_area'] = max_area
-		item['min_price'] = min_price
-		item['max_price'] = max_price
+		item['min_booking_price'] = min_booking_price
+		item['max_booking_price'] = max_booking_price
+		item['min_resale_price'] = min_resale_price
+		item['max_resale_price'] = max_resale_price
 		item['amenity'] = amenity
 		item['address'] = addres.encode('utf8')
-		item['detail'] = det
-		pprint(item)
+		item['units'] = det
+		item['website']  = (response.url).split('/')[2].split('.')[1]
+		item['project_detail'] = project_detail.encode('utf8')
+		item['builderName'] = builderName.encode('utf-8')
+
+		fire = firebase.FirebaseApplication('https://abcapp-8345a.firebaseio.com/',None)
+		print fire.put('/','temp',item)
+		return
